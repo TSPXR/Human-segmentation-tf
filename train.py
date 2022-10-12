0f8a63733a41fe38c7956d975aea8f213739e30e
park <chansoo0710@gmail.com>
2022-10-12 19:23:34 +0900

    test home

--- train.py
+++ train.py
@@ -32,7 +32,7 @@ parser.add_argument('--saved_model_path', type=str,   help='Saved model weight p
 # Set Training Options
 # Multi-adam-b16-e150-lr0.005-focal2.0-augment-boundary20_aux0.4-weightDecay
 parser.add_argument('--model_prefix',     type=str,    help='Model name',
-                    default='light_pidnet-b16-ep100-lr0.001-ce2+aux0.4+boundary2-adam-640x360-singleGpu-semanticSeg-shift_aug_bn0.9')
+                    default='pidnet-b16-ep100-lr0.001-ce2+aux0.4+boundary2-adam-640x360-singleGpu-semanticSeg-shift_aug_bn0.9')
 parser.add_argument('--batch_size',       type=int,    help='Batch size per each GPU',
                     default=16)
 parser.add_argument('--epoch',            type=int,    help='Training epochs',
